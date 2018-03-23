aba7fb175e37157204254c1d862415f1e863dde6
Girish Kumar <girish.k38@yahoo.com>
2018-03-24 02:19:03 +0530

    login script

---
+++ phpscript/login_submit.php
@@ -0,0 +1,27 @@
+<?php
+require_once "../includes/common.php";
+
+if(isset($_POST['email']) && isset($_POST['password'])){
+    $pass = mysqli_real_escape_string($con, $_POST['password']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $query = "SELECT password, name from user WHERE email = '$email'";
+    $result = $con->query($query);
+    if($result && $result->num_rows == 1){
+        $row = $result->fetch_assoc();
+        if(md5($pass) == $row['password']){
+            session_start();
+            $_SESSION['email'] = $email;
+            $_SESSION['name'] = $row['name'];
+            header("Location: ../index.php");
+        }else{
+            header("Location: ../login.php?error=Wrong password");
+        }
+    }else{
+        header("Location: ../login.php?error=Email does not exist");
+    }
+}else{
+    header("Location: ../login.php?error=email or password is wrong");
+}
+
+
+?>
